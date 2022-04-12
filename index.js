9fe8bfb479b0111803a4978cb50ded0c042afa4a
Joey Lipton <harmansohi16@gmail.com>
2022-04-12 12:45:59 -0400

    ye 2

---
+++ index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+require("dotenv").config();
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(
+    MONGO_URI,
+    { useUnifiedTopology : true }
+);
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+    console.log("Successfully connected to MongoDB.");
+});
