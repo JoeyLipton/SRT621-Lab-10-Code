bef2ecc574c729ae6ff692749cbf0f731922c6fd
Joey Lipton <harmansohi16@gmail.com>
2022-04-13 09:21:14 -0400

    hopefully finally

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,8 @@
 const path = require("path");
 
+const mongoose = require('mongoose'),
+    Books = require("../models/books")
+
 exports.sendHome = (req, res) => {
     res.redirect("index");
 }
@@ -8,7 +11,70 @@ exports.sendIndex = (req, res) => {
     res.render("index");
 }
 
+
+
+exports.getAllBooks = (req, res, next) => {
+    Books.find({}, (err, books) => {
+        if (err) next(err);
+        req.data = books;
+        next();
+    });
+};
+
 exports.sendBook = (req, res) => {
-    const book = req.params.book;
-    res.render(book);
+    const bookNumber = req.params.bookNumber;
+    switch (bookNumber) {
+        case "1":
+            const bookTitle1 = Books.find({
+                bookTitle: "Compilers: Principles, Tools, and Techniques"
+            })
+            bookTitle1.exec((err, data) => {
+                if (!err) {
+                    res.render(bookNumber, {
+                        book: data
+                    });
+                }
+                else {
+                    return handleError(err);
+                }
+            })
+            break;
+        case "2":
+            const bookTitle2 = Books.find({
+                bookTitle: "Data Structures and Algorithms in Java "
+            })
+            bookTitle2.exec((err, data) => {
+                if (!err) {
+                    res.render(bookNumber, {
+                        book: data
+                    });
+                }
+                else {
+                    return handleError(err);
+                }
+            })
+            break;
+        case "3":
+            const bookTitle3 = Books.find({
+                bookTitle: "The Cat in the Hat"
+            })
+            bookTitle3.exec((err, data) => {
+                if (!err) {
+                    res.render(bookNumber, {
+                        book: data
+                    });
+                }
+                else {
+                    return handleError(err);
+                }
+            })
+            break;
+        default: 
+            Books.find({bookTitle: "Compilers: Principles, Tools, and Techniques"})
+                .then((result) => {
+                    res.render(bookNumber);
+                })
+                break;
+    }
+    // res.render(bookNumber);
 }
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express'),
     app = express(),
     controller = require('./controllers/homeController')
 
+const layouts = require("express-ejs-layouts");
+
 const mongoose = require('mongoose'),
-    books = require("./models/books")
+    Books = require("./models/books")
 
 require("dotenv").config();
 const MONGO_URI = process.env.MONGO_URI;
@@ -15,13 +17,44 @@ mongoose.connect(
     .catch((err) => console.log(err));
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static("public"))
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(layouts);
+
+app.get("/home", controller.getAllBooks, (req, res, next) => {
+    res.render("index", { books: req.data }
+    )
+})
 
 app.get("/", controller.sendIndex);
 app.get("/home", controller.sendIndex);
-app.get("/books/:bookNumber", controller.sendHome);
+app.get("/books/:bookNumber", controller.sendBook);
+
+
+`
+app.get('/all-blogs', (req, res) => {
+    Books.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
+`
+
+app.get('/single-blog', (req, res) => {
+    Books.find({
+                bookTitle: "Compilers: Principles, Tools, and Techniques"
+            })
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
 
 app.listen(3000);
 
